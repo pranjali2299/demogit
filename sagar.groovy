e9cee62ce61fcaccdee5bbb41cc75de64e36d94f
pranjali2299 <99962858+pranjali2299@users.noreply.github.com>
2023-01-19 15:32:20 +0530

    Update sagar.groovy

--- sagar.groovy
+++ sagar.groovy
@@ -6,7 +6,7 @@ pipeline {
       stage('git-pull')
         steps { 
           git credentialsId: 'e247ea7b-e6b8-4aff-90ea-38e5566b78c0', url: 'git@github.com:pranjali2299/demogit.git'
-          sh echo Hello
+          sh echo 'Hello'
         }
     }
     stage('Build') { 
@@ -18,10 +18,11 @@ pipeline {
       steps {
            echo 'git test'
       }
+    }
        stage('Deploy') { 
       steps {
            echo 'git deploy'
        } 
      } 
    }
-}
+
