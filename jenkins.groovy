98ec3208716dbab14c7292baa9b333d10ac2c2e1
pranjali2299 <99962858+pranjali2299@users.noreply.github.com>
2023-01-20 15:39:38 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('git-pull') { 
             steps {
-               git credentialsId: 'node', url: 'https://github.com/pranjali2299/demogit.git'
+               git credentialsId: 'a7224201-f83c-4f3b-b4f6-53061054b227', url: 'git@github.com:pranjali2299/demogit.git'
             }
         }
     }
